b4b35ad453416a574572f57fe39bb7a6772a4f54
Bruce <t15thbruce@outlook.de>
2018-12-19 12:30:11 -0800

    logic for some alleles

--- script.py
+++ script.py
@@ -7,10 +7,15 @@ print('\n')
 mother = input("Input full mother genotype here: ")
 print('\n')
 
+# TODO: define implementation
+def generate_gametes(genotype):
+    pass
 
-# def generate_gametes(genotype):
-# do something
+# TODO: recognize phenotypes using given offspring functions
 
+# TODO: split genotypes into seperate traits
+
+# implementation done
 def generate_offsprings(father_gametes, mother_gametes):
     offspring = []
 
@@ -20,21 +25,37 @@ def generate_offsprings(father_gametes, mother_gametes):
 
     return offspring
 
-
+# implementation done
 def prettify_offspring(offsprings):
-
     to_return = []
     for offspring in offsprings:
-        sorted_offspring = sorted(list(offspring), key= lambda x:(x.lower(),x))
+        sorted_offspring = sorted(list(offspring), key= lambda x:(x.lower(),x)) # sorts so it appears like a genotype
         sorted_string = ''.join(sorted_offspring)
         to_return.append(sorted_string)
     to_return.sort()
     return to_return
 
+# tall = allele Tt
+# returns true for dominant, false for recessive
+def check_tall(trait):
+    if trait.contains('T'):
+        return 1
+    else:
+        return 0
 
-# declare strings for different cases in each allele
-# for example:
-# incoC = "CW"
+# RR is right handed (1)
+# LL is left handed (2)
+# RL is ambidextrous (0)
+def check_handed(trait):
+    if trait == 'RR':
+        return 1
+    if trait == 'LL':
+        return 2
+    if trait == 'RL':
+        return 0
+
+# hair: Black > Brown > Blonde > Red > Purple
+ 
 
 offspring = generate_offsprings(['AB', 'Ab', 'aB', 'ab'], ['AB', 'Ab', 'aB', 'ab'])
 genotypes = prettify_offspring(offspring)
